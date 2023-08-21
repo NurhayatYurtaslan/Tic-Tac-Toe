ee15f6b068515001dc2160911d2c2952ff12bfa4
NurhayatYurtaslan <80510115+NurhayatYurtaslan@users.noreply.github.com>
2023-08-22 02:20:00 +0300

    update 🆕: Container to Scaffold

--- tic_tac_toe_game/lib/game_screen.dart
+++ tic_tac_toe_game/lib/game_screen.dart
@@ -11,6 +11,6 @@ class GameScreen extends StatefulWidget {
 class _GameScreenState extends State<GameScreen> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold();
   }
 }
