127497e8b784df7ce6786756b47f8428b1991417
NurhayatYurtaslan <80510115+NurhayatYurtaslan@users.noreply.github.com>
2023-08-21 22:54:49 +0300

    add ✨: player 1 and player 2 name boxes

--- tic_tac_toe_game/lib/home_screen.dart
+++ tic_tac_toe_game/lib/home_screen.dart
@@ -1,14 +1,14 @@
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatefulWidget {
-  const HomeScreen({super.key});
+  const HomeScreen({Key? key}) : super(key: key);
 
   @override
   State<HomeScreen> createState() => _HomeScreenState();
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-  final GlobalKey<FormState> _fromkey = GlobalKey<FormState>();
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
   final TextEditingController player1Controller = TextEditingController();
   final TextEditingController player2Controller = TextEditingController();
 
@@ -17,7 +17,7 @@ class _HomeScreenState extends State<HomeScreen> {
     return Scaffold(
       backgroundColor: Color(0xFF323D5B),
       body: Form(
-        key: _fromkey,
+        key: _formKey,
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
@@ -49,7 +49,44 @@ class _HomeScreenState extends State<HomeScreen> {
                   errorBorder: OutlineInputBorder(
                     borderSide: BorderSide(color: Colors.red),
                   ),
+                  hintText: "Player 1 Name",
+                  hintStyle: TextStyle(color: Colors.white),
                 ),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return ("Please enter player 1 name");
+                  }
+                  return null;
+                },
+              ),
+            ),
+            SizedBox(height: 20),
+            Padding(
+              padding: EdgeInsets.all(15),
+              child: TextFormField(
+                controller: player2Controller,
+                style: TextStyle(color: Colors.white),
+                decoration: InputDecoration(
+                  focusedBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.white)),
+                  focusedErrorBorder: OutlineInputBorder(
+                    borderSide: BorderSide(color: Colors.red),
+                  ),
+                  enabledBorder: OutlineInputBorder(
+                    borderSide: BorderSide(color: Colors.white),
+                  ),
+                  errorBorder: OutlineInputBorder(
+                    borderSide: BorderSide(color: Colors.red),
+                  ),
+                  hintText: "Player 2 Name",
+                  hintStyle: TextStyle(color: Colors.white),
+                ),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return ("Please enter player 2 name");
+                  }
+                  return null;
+                },
               ),
             ),
           ],
@@ -57,6 +94,4 @@ class _HomeScreenState extends State<HomeScreen> {
       ),
     );
   }
-
-  borderSide({required Color color}) {}
 }
